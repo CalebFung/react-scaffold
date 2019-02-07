0be30416905a333466f3f79db12135ae0d64b7cb
Caleb Fung <caleb.t.fung@gmail.com>
2019-02-07 04:46:35 -0600

    Updates

---
+++ precache-manifest.d51cfcc2c58ba62a25cc2ebd3c9afcd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0402abb7e395a14641e604c564446b0",
+    "url": "/react-scaffold/static/media/logo.a0402abb.png"
+  },
+  {
+    "revision": "526bcd9cf7da60870e00",
+    "url": "/react-scaffold/static/js/runtime~main.526bcd9c.js"
+  },
+  {
+    "revision": "291e6ae2172f33851eea",
+    "url": "/react-scaffold/static/js/main.291e6ae2.chunk.js"
+  },
+  {
+    "revision": "fc7ce847abc8e435798f",
+    "url": "/react-scaffold/static/js/1.fc7ce847.chunk.js"
+  },
+  {
+    "revision": "291e6ae2172f33851eea",
+    "url": "/react-scaffold/static/css/main.5fc2f23e.chunk.css"
+  },
+  {
+    "revision": "e786ef90fb25246ac08096f0823288d5",
+    "url": "/react-scaffold/index.html"
+  }
+];
